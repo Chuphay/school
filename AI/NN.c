e7bf53f7926a6c7f835c5db213a292506a9b979b
Chuphay <chuphay@gmail.com>
2014-10-17 11:01:58 -0700

    makefile with math

--- AI/NN.c
+++ AI/NN.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
@@ -7,5 +8,13 @@ int main(){
   printf("%d\n",c);
   char m = getchar();
   printf("%d\n", m-'0');
+  
+
+  double x = 0;
+  
+  printf("The exponential value of %lf is %lf\n", x, exp(x));
+  printf("The exponential value of %lf is %lf\n", x+1, exp(x+1));
+  printf("The exponential value of %lf is %lf\n", x+2, exp(x+2));
+
   return 0;
 }
