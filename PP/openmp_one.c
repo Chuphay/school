68def3e0c57fcac4c1b5dca53e223e2bd608972b
Chuphay <chuphay@gmail.com>
2015-01-06 21:46:19 -0800

    three different things all working

---
+++ PP/openmp_one.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(void){
+  printf("hello\n");
+  return 0;
+}
---
+++ PP/pthread_one.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(void){
+  printf("hello/n");
+  return 0;
+}
