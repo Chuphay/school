af4faf741064aae1672400778f9536f71387baa1
Chuphay <chuphay@gmail.com>
2015-01-10 16:25:47 -0800

    working on my parallel programming

--- PP/pthread_matrix.c
+++ PP/pthread_matrix.c
@@ -4,6 +4,10 @@
 
 //gcc -g -Wall -o pthread_matrix pthread_matrix.c -lpthread
 
+//typical use case:
+//./pthread_matrix 4 2
+//will have 4 threads and multiply a 2X2 matrix
+
 void *h(void *rank){
   int my_rank = (int) rank;
   printf("h %d\n", my_rank);
@@ -22,29 +26,31 @@ double sum(double *v, int n){
 int main(int argc, char *argv[]){
 
 
-  if(argc != 2){
-    printf("Error. Must supply # of threads. Exiting...\n");
+  if(argc != 3){
+    printf("Error. Must supply # of threads and dimension of matrix. Exiting...\n");
     exit(1);
   }
 
   int thread_count = atoi(argv[1]);
+  int n = atoi(argv[2]);
   pthread_t *threads = malloc(thread_count*sizeof(pthread_t));
 
-  double A[thread_count][thread_count], x[thread_count], y[thread_count];
+  
+  double A[n][n], x[n], y[n];
   int i,j;
-  for(i = 0; i<thread_count; i++){
+  for(i = 0; i<n; i++){
     x[i]= 1;
-    for(j = 0; j<thread_count; j++){
+    for(j = 0; j<n; j++){
       A[i][j] = j+1;
     }
   }
-  for(i = 0; i<thread_count; i++){
+  for(i = 0; i<n; i++){
     y[i]= 0;
-    for(j = 0; j<thread_count; j++){
+    for(j = 0; j<n; j++){
       y[i] += A[i][j]*x[j];
     }
   }
-  printf("sum = %f\n", sum(y,thread_count));
+  printf("sum = %f\n", sum(y, n));
 
 
   double my_rand = rand() / ((double)RAND_MAX+1.0); 
