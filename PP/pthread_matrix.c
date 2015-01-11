07fc79af8716bf9cc553e248f4e9d009e8864ef3
Chuphay <chuphay@gmail.com>
2015-01-10 17:29:13 -0800

    yay! finished the matrix parallel program

--- PP/pthread_matrix.c
+++ PP/pthread_matrix.c
@@ -5,14 +5,43 @@
 //gcc -g -Wall -o pthread_matrix pthread_matrix.c -lpthread
 
 //typical use case:
-//./pthread_matrix 4 2
-//will have 4 threads and multiply a 2X2 matrix
+//./pthread_matrix 4
+
+
+//We'll use some global variables as suggested by my book
+#define N 43
+int thread_count;
+double A[N][N], x[N], y[N];
 
 void *h(void *rank){
+  int extra = N%thread_count;
+  int count = N/thread_count;
   int my_rank = (int) rank;
-  printf("h %d\n", my_rank);
+  int i,j, start, end;
+  if(extra - my_rank>0){
+    start = my_rank*count + my_rank;
+    end =  (my_rank+1)*count+my_rank+1;
+    for(i = start; i < end; i++){
+      y[i]= 0;
+      for(j = 0; j<N; j++){
+	y[i] += A[i][j]*x[j];
+      }
+    }
+  } else {
+    start =  my_rank*count + extra;
+    end =  (my_rank+1)*count+extra;
+    for(i = start; i < end; i++){
+      y[i]= 0;
+      for(j = 0; j<N; j++){
+	y[i] += A[i][j]*x[j];
+      }
+    }
+
+  }
+
   return NULL;
-} 
+}
+ 
 
 double sum(double *v, int n){
   int i;
@@ -26,18 +55,18 @@ double sum(double *v, int n){
 int main(int argc, char *argv[]){
 
 
-  if(argc != 3){
-    printf("Error. Must supply # of threads and dimension of matrix. Exiting...\n");
+  if(argc != 2){
+    printf("Error. Must supply # of threads. Exiting...\n");
     exit(1);
   }
 
-  int thread_count = atoi(argv[1]);
-  int n = atoi(argv[2]);
+  thread_count = atoi(argv[1]);
   pthread_t *threads = malloc(thread_count*sizeof(pthread_t));
 
   
-  double A[n][n], x[n], y[n];
+
   int i,j;
+  int n = N;
   for(i = 0; i<n; i++){
     x[i]= 1;
     for(j = 0; j<n; j++){
@@ -51,20 +80,23 @@ int main(int argc, char *argv[]){
     }
   }
   printf("sum = %f\n", sum(y, n));
-
-
-  double my_rand = rand() / ((double)RAND_MAX+1.0); 
-  printf("my_rand = %f\n", my_rand);
-
-  
-
+  for(i = 0; i<n; i++){
+    y[i]= 0; //clear y just to make sure my algorithm is working
+  }
+ 
   for(i = 0; i< thread_count; i++){
     pthread_create(&threads[i], NULL, h, (void*) i);
   } 
   for(i = 0; i< thread_count; i++){
     pthread_join(threads[i], NULL);
   }
+ 
+  printf("sum = %f\n", sum(y, n));
+
+
 
   free(threads);
   return 0;
 } 
+
+
